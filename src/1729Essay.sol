3326334f2d9b85781e39b0df771adbba9129c079
neodaoist <tom@loudverse.com>
2022-07-12 12:56:27 -0400

    :bulb: Add initial NatSpec documentation to contracts

--- src/1729Essay.sol
+++ src/1729Essay.sol
@@ -3,10 +3,18 @@ pragma solidity ^0.8.15;
 
 import {ERC721} from "solmate/tokens/ERC721.sol";
 
+/// @title An essay from the 1729 writers union
+/// @author neodaoist, plaird
+/// @notice A 1729w admin can mint and burn essay NFTs on this contract
+/// @dev XYZ
 contract OneSevenTwoNineEssay is ERC721 {
 
     constructor() ERC721("1729 Essay", "1729ESSAY") {}
 
+    /// @notice Get the Essay NFT metadata URI
+    /// @dev XYZ
+    /// @param id The Token ID for a specific Essay NFT
+    /// @return Fully-qualified URI of an Essay NFT, e.g., XYZ
     function tokenURI(uint256 id) public view override returns (string memory) {
         return "";
     }
--- src/1729ProofOfContribution.sol
+++ src/1729ProofOfContribution.sol
@@ -3,11 +3,19 @@ pragma solidity ^0.8.15;
 
 import {ERC1155} from "solmate/tokens/ERC1155.sol";
 
+/// @title A proof of contribution by members of the 1729 writers union
+/// @author neodaoist, plaird
+/// @notice A 1729w admin can issue proof of contribution tokens on this contract
+/// @dev XYZ
 contract OneSevenTwoNineProofOfContribution is ERC1155 {
 
     constructor() {}
     
-    function uri(uint256) public view override returns (string memory) {
+    /// @notice Get the Proof of Contribution token metadata URI
+    /// @dev XYZ
+    /// @param id The Token ID for a specific Proof of Contribution token
+    /// @return Fully-qualified URI of a Proof of Contribution token, e.g., XYZ
+    function uri(uint256 id) public view override returns (string memory) {
         return "";
     }
 }
