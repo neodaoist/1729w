7656ca9f15c7ca4d06a1d54e62b7557d93704df1
neodaoist <tom@loudverse.com>
2022-07-14 12:39:10 -0400

    :microscope: Rev on 1155-flavored SBT / cryptocredential idea

--- src/SBT.sol
+++ src/SBT.sol
@@ -3,44 +3,48 @@ pragma solidity ^0.8.15;
 
 /// @title An idea for an 1155-flavored SBT / cryptocredential standard
 /// @author neodaoist
-/// @notice xyz
-/// @dev Just exploring now, Keeping it simple and clear
-abstract contract SBT {
+/// @notice Just exploring for now, Keeping it simple and clear
+/// @dev Further ideas — 3rd-Party Attestations, Hubs and Authorities, Reveal/Conceal Pattern
+interface SBT {
     //
 
     /*//////////////////////////////////////////////////////////////
                         Events
     //////////////////////////////////////////////////////////////*/
 
-    event TransferSingle(address indexed _operator, address indexed _from, address indexed _to, uint256 _tokenID, uint256 _value);
+    event IssueSingle(address indexed _issuer, address indexed _issuee, uint256 _tokenID, uint256 _value);
 
-    event TransferBatch(address indexed _operator, address indexed _from, address indexed _to, uint256[] _tokenIDs, uint256[] _values);
+    event IssueBatch(address indexed _issuer, address indexed _issuee, uint256[] _tokenIDs, uint256[] _values);
+
+    event RevokeSingle(address indexed _revoker, address indexed _revokee, uint256 _tokenID, uint256 _value);
+
+    event RevokeBatch(address indexed _revoker, address indexed _revokee, uint256[] _tokenIDs, uint256[] _values);
 
     /*//////////////////////////////////////////////////////////////
                         Views
     //////////////////////////////////////////////////////////////*/
 
-    function balanceOf(address _owner, uint256 _tokenID) external virtual view returns (uint256);
+    function hasToken(address _owner, uint256 _tokenID) external view returns (bool);
 
-    function balanceOfBatch(address[] calldata _owners, uint256[] calldata _tokenIDs) external virtual view returns (uint256[] memory);
+    function hasTokenBatch(address[] calldata _owners, uint256[] calldata _tokenIDs) external view returns (bool[] memory);
 
-    function allOwnersOf(uint256 _tokenID) external virtual view returns (address[] memory);
+    function allOwnersOf(uint256 _tokenID) external view returns (address[] memory);
 
-    function allTokensOf(address _owner) external virtual view returns (uint256[] memory);
+    function allTokensOf(address _owner) external view returns (uint256[] memory);
 
     /*//////////////////////////////////////////////////////////////
                         Issuing
     //////////////////////////////////////////////////////////////*/
 
-    function issue(address _recipient, uint256 _tokenID) external virtual;
+    function issue(address _recipient, uint256 _tokenID) external;
 
-    function issueBatch(address[] calldata _recipients, uint256[] calldata _tokenIDs) external virtual;
+    function issueBatch(address[] calldata _recipients, uint256[] calldata _tokenIDs) external;
 
     /*//////////////////////////////////////////////////////////////
                         Revoking
     //////////////////////////////////////////////////////////////*/
 
-    function revoke(address _recipient, uint256 _tokenID) external virtual;
+    function revoke(address _recipient, uint256 _tokenID) external;
 
-    function revokeBatch(address[] calldata _recipients, uint256[] calldata _tokenIDs) external virtual;
+    function revokeBatch(address[] calldata _recipients, uint256[] calldata _tokenIDs) external;
 }
