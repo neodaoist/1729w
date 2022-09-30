0f70718728e1c348cc167fb0fbc0e812f3e14124
Peter Laird <plaird@planview.com>
2022-09-30 12:32:38 -0600

    Started refactor to stepdefs.  Ready to discuss at standup

--- tests/writers.rs
+++ tests/writers.rs
@@ -1,5 +1,5 @@
+use std::borrow::Borrow;
 use std::convert::Infallible;
-
 use async_trait::async_trait;
 use cucumber::{gherkin::Step, given, when, then, World, WorldInit, Cucumber};
 use std::collections::HashMap;
@@ -7,6 +7,7 @@ use std::fmt::Formatter;
 use std::panic::AssertUnwindSafe;
 use std::sync::Arc;
 use std::time::Duration;
+use std::env;
 use async_std::task;
 use ethers::prelude::LocalWallet;
 use futures::FutureExt;
@@ -18,6 +19,8 @@ use ethers_middleware::signer::SignerMiddleware;
 use ethers_providers::{Provider, Http, Middleware};
 use ethers::signers::Signer;
 use ethers::prelude::Wallet;
+use ethers::abi::Uint;
+
 // use ethers_signers::wallet;
 
 // 
@@ -133,24 +136,47 @@ impl World for WriterWorld {
     }
 }
 
+pub trait AnvilUtil {
+    fn get_writer_wallet(&self) -> LocalWallet;
+}
+
+impl AnvilUtil for WriterWorld {
+    fn get_writer_wallet(&self) -> LocalWallet {
+        let anvil = self.anvil.as_ref().unwrap().anvil.borrow();
+        let wallet: LocalWallet = anvil.keys()[0].clone().into();
+        wallet
+    }
+}
+
+
 
 #[tokio::main]
 // Test runner
-async fn main() -> eyre::Result<()> {
+async fn main() -> eyre::Result<()>
+{
 
     let world = WriterWorld::cucumber()
         // Start a fresh anvil before each scenario
         .before(move |_, _, _, world| {
             async move {
+                let fork_endpoint = env::var("ETH_NODE_URL").expect("Environment variable ETH_NODE_URL should be defined and be a valid API URL");
                 let anvil = Anvil::new()
+                    .fork(fork_endpoint)
+                    .chain_id(31337_u64)
                     .spawn();
                 let endpoint = anvil.endpoint();
-                println!("Anvil running at `{}`", endpoint);
-/*
+                println!("Anvil running at `{}` with chain_id `{}`", endpoint, anvil.chain_id());
+
                 let provider = Provider::<Http>::try_from(anvil.endpoint()).expect("Failed to connect to Anvil").interval(Duration::from_millis(10u64));
 
                 let wallet: LocalWallet = anvil.keys()[0].clone().into();
                 let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id())));
+                let connection = AnvilConnection{
+                    anvil: anvil,
+                    client: client
+                };
+                world.anvil = Option::Some(connection);
+                /*
 //                let nft_contract = task::block_on(SevenTeenTwentyNineEssay::deploy(client, ()).expect("Failed to deploy").send()).expect("Failed to send");
 
 
@@ -163,11 +189,8 @@ async fn main() -> eyre::Result<()> {
 */
             }.boxed()
         })
-        .run_and_exit("tests/features/implemented");
-
-    //FIXME -- Disables tests
-    //let err = AssertUnwindSafe(world).catch_unwind().await.expect_err("should_err");
-    //warn!("Error: {}", err.downcast_ref::<String>().unwrap());
+        .run_and_exit("tests/features/implemented")
+        .await;
 
     Ok(())
 }
@@ -363,33 +386,28 @@ fn vote_scenario_2_then_1(world: &mut WriterWorld, voter_account: String, vote_c
 
 #[given(regex = r"^The Essay NFT contract is deployed$")]
 fn publish_given_1(world: &mut WriterWorld) {
-    /*
-        let anvil = &world.anvil.as_ref().expect("Anvil should be initialized").anvil;
-        let wallet: LocalWallet = anvil.keys()[0].clone().into();
-        let provider = Provider::<Http>::try_from(anvil.endpoint()).expect("Failed to connect to Anvil").interval(Duration::from_millis(10u64));
 
-        let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id())));
-        // let nft_contract = task::block_on(SevenTeenTwentyNineEssay::deploy(client, ()).expect("Failed to deploy").send()).expect("Failed to send");
-        let factory = ethers::contract::ContractFactory::new(
-            SEVENTEENTWENTYNINEESSAY_ABI.clone(),
-            SEVENTEENTWENTYNINEESSAY_BYTECODE.clone().into(),
-            client,
-        );
-        let deployer_result = factory.deploy(());
-        let deployer = match deployer_result {
-            Ok(deployer) => deployer,
-            Err(error) => panic!("Error result from deploy: {:?}", error),
-        };
-    */
-    //let deployer: ethers::contract::ContractDeployer<SignerMiddleware<ethers_providers::Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>, ethers::contract::Contract<SignerMiddleware<ethers_providers::Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>>> = ethers::contract::ContractDeployer::new(deployer);
 
-    // world.nft_contract = Some(nft_contract);
+    // Deploy contract
+    let anvil = world.anvil.as_ref().unwrap().anvil.borrow();
+    let provider = Provider::<Http>::try_from(anvil.endpoint()).expect("Failed to connect to Anvil").interval(Duration::from_millis(10u64));
+
+    let wallet: LocalWallet = anvil.keys()[0].clone().into();
+    let multisig = wallet.address();
+    let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id())));
+
+    let nft_contract = task::block_on(SevenTeenTwentyNineEssay::deploy(client, multisig).expect("Failed to deploy").send()).expect("Failed to send");
+    world.nft_contract = Option::Some(nft_contract);
 
 }
 
 #[given(regex = r"^there are no NFTs minted on the contract$")]
-fn publish_given_2(world: &mut WriterWorld) {
-    // TODO: just for clarity
+async fn publish_given_2(world: &mut WriterWorld) {
+        // Query Total Supply
+    let nft_contract = world.nft_contract.as_ref().unwrap();
+    let total_supply = nft_contract.method::<_, Uint>("totalSupply", ())
+        .expect("Error finding method").call().await.expect("Error sending total supply call");
+    assert_eq!(Uint::from(0), total_supply);
 }
 
 #[given(regex = r"^the Cohort is ([\d]+)$")]
@@ -429,8 +447,54 @@ fn publish_given_9(world: &mut WriterWorld, essay_votes: String) {
 }
 
 #[when("I mint, list, and bid on the Essay NFT")]
-fn publish_when_1(world: &mut WriterWorld) {
-    // TODO: Unimplemented
+async fn publish_when_1(world: &mut WriterWorld) {
+    /*
+    // Mint Essay NFT
+    let anvil = world.anvil.as_ref().unwrap().anvil.borrow();
+    let client = world.anvil.as_ref().unwrap().client.clone();
+    let multisig = world.getMultisigWallet().address();
+
+    let nft_contract = world.nft_contract.as_ref().unwrap();
+    let mint_call = nft_contract.mint(multisig, "https://test.com/test".to_string());
+    task::block_on(mint_call.send()).expect("Failed to send mint transaction");
+
+        // List
+    let provider = Provider::<Http>::try_from(anvil.endpoint()).expect("Failed to connect to Anvil").interval(Duration::from_millis(10u64));
+    let wallet: LocalWallet = anvil.keys()[0].clone().into();
+    let client = Arc::new(SignerMiddleware::new(provider, wallet.with_chain_id(anvil.chain_id())));
+    let zora_address: ethers::core::types::Address = "0x5f7072E1fA7c01dfAc7Cf54289621AFAaD2184d0".parse::<Address>().expect("Couldn't parse zora address");
+    let transfer_helper_address: H160 = "0x909e9efE4D87d1a6018C2065aE642b6D0447bc91".parse::<Address>().expect("Couldn't parse transfer helper address");
+    let module_manager_address: H160 = "0x850A7c6fE2CF48eea1393554C8A3bA23f20CC401".parse::<Address>().expect("Couldn't parse transfer helper address");
+
+
+    // Approve
+    let reserve_auction_contract :Contract<SignerMiddleware<ethers_providers::Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>> = Contract::new(zora_address, RESERVEAUCTIONCOREETH_ABI.clone(), client.clone());
+    let approve_call = nft_contract.set_approval_for_all(transfer_helper_address, true);
+    task::block_on(approve_call.send()).expect("Failed to send approve transaction");
+
+    let module_manager_contract :Contract<SignerMiddleware<ethers_providers::Provider<Http>, Wallet<ethers::core::k256::ecdsa::SigningKey>>> = Contract::new(module_manager_address, MODULEMANAGER_ABI.clone(), client.clone());
+    module_manager_contract.method::<_, ()>("setApprovalForModule", (zora_address, true))
+        .expect("Error finding setApprovalForModule method").send().await.expect("Error calling approval for module");
+
+    // Create auction
+    println!("Creating listing");
+    let auction_duration: ethers::prelude::U256 = ethers::core::types::U256::from(3*24*3600); // 3 days
+    let auction_reserve_price: ethers::prelude::U256 = ethers::core::types::U256::from_dec_str("10000000000000000").expect("Reserve price should be a valid number");
+
+    reserve_auction_contract.method::<_, ()>("createAuction", (nft_contract.address(), token_id, auction_duration, auction_reserve_price, multisig, block_uint256))
+        .expect("Error finding createAuction method").send().await.expect("Error calling createAuction");
+
+        // Place bid
+    println!("Placing bid");
+    let bid_value = ethers::types::U256::from_dec_str("20000000000000000").expect("Value should parse");
+    reserve_auction_contract.method::<_, ()>("createBid", (nft_contract.address(), token_id))
+        .expect("Error finding createAuction method")
+        .value(bid_value)
+        .send().await.expect("Error calling createAuction");
+        
+     */
+
+
 }
 
 #[then("There should be PLACEHOLDER STEP XYZ")]
