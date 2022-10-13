1ef4ff30aea83fd3b7ec23b56c471e8269bfac6b
Peter Laird <plaird@planview.com>
2022-10-12 21:34:50 -0600

    Lock solidity pragma versions for mainnet deploy readiness

--- src/1729Essay.sol
+++ src/1729Essay.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
 import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
 import {Ownable} from "openzeppelin-contracts/access/Ownable.sol";
